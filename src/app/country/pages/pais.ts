f3bca53647cf4880302fb3d834804926c8662f0c
Alfredo Ortega Elizarrarás <aortegae@SCJNHTPME0548.scjn.PJF.gob.mx>
2026-03-18 20:54:24 -0600

    Paginas pais y region

--- src/app/country/pages/pais.ts
+++ src/app/country/pages/pais.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import CountrySearchComponent from '../components/search';
+import CountryListComponent from '../components/country-list';
 
 @Component({
   selector: 'country-pais',
   templateUrl: './pais.html',
+  imports: [CountrySearchComponent, CountryListComponent],
 })
 export default class CountryPaisPageComponent {}
--- src/app/country/pages/region.ts
+++ src/app/country/pages/region.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import CountryListComponent from '../components/country-list';
 
 @Component({
   selector: 'country-region',
   templateUrl: './region.html',
+  imports: [CountryListComponent],
 })
 export default class CountryRegionPageComponent {}
